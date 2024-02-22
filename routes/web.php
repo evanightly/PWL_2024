f56328998962b83578c6da8eaddcd50e63200d1a
Galur Arasy <adamhenderson3x3@gmail.com>
2024-02-22 16:25:29 +0700

    Pertemuan 2 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\Test;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +18,54 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
+
+Route::get('/hello', function () {
+    return 'Hello';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2141762136 <br> Nama : Galur Arasy L.';
+});
+
+Route::get('posts/{id}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post ke-' . $postId . ' Komentar ke-' . $commentId;
+});
+
+Route::get('article/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get(
+    '/user/profile',
+    [UserProfileController::class, 'show']
+)->name('profile');
+
+Route::get('user/{name?}', function ($name = null) {
+    return 'Nama Saya : ' . $name;
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        return 'Account : ' . $account . ' ID : ' . $id;
+    });
+});
+
+Route::get('/test', function () {
+    return 'ok';
+})->middleware([Test::class]);
+
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Route::redirect('ok', 'test');
+
+Route::view('/welcome', 'welcome', ['name' => 'Galur Arasy L.']);
